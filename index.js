dffe8efed8d4138f38493e64a66859c09ad5adbd
Daniel Calvin <arockne2003@yahoo.com>
2021-08-16 09:44:04 -0700

    Implement renderBeers

--- index.js
+++ index.js
@@ -7,7 +7,24 @@
 function getBeers(page = 1) {
   fetch(`https://api.punkapi.com/v2/beers`)
   .then(resp => resp.json())
-  .then(beers => console.log(beers))
+  .then(renderBeers)
+}
+
+function renderBeers(beers) {
+  beers.forEach(beer => {
+    const img = document.createElement('img');
+    img.src = beer.image_url;
+
+    const name = document.createElement('h3');
+    name.textContent = beer.name;
+
+    const card = document.createElement('div')
+    card.className = 'card';
+    card.append(img, name);
+    console.log(card);
+
+    document.querySelector('#item-container').appendChild(card)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
