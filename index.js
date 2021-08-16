22612597d4ee44a5658b52a6c0af14de76caf72d
Daniel Calvin <arockne2003@yahoo.com>
2021-08-16 10:21:46 -0700

    Implement createBeerCard

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-
-//i
-  //page number
-//o
-  //none
-//getBeers
 function getBeers(page = 1) {
   fetch(`https://api.punkapi.com/v2/beers`)
   .then(resp => resp.json())
@@ -11,20 +5,22 @@ function getBeers(page = 1) {
 }
 
 function renderBeers(beers) {
-  beers.forEach(beer => {
-    const img = document.createElement('img');
-    img.src = beer.image_url;
+  beers.forEach(createBeerCard)
+}
 
-    const name = document.createElement('h3');
-    name.textContent = beer.name;
-
-    const card = document.createElement('div')
-    card.className = 'card';
-    card.append(img, name);
-    console.log(card);
-
-    document.querySelector('#item-container').appendChild(card)
-  })
+function createBeerCard(beer) {
+  const img = document.createElement('img');
+  img.src = beer.image_url;
+  
+  const name = document.createElement('h3');
+  name.textContent = beer.name;
+  
+  const card = document.createElement('div')
+  card.className = 'card';
+  card.append(img, name);
+  console.log(card);
+  
+  document.querySelector('#item-container').appendChild(card)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
